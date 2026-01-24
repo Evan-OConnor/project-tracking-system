2b35dbe5ad48dfdfb81c45c381a319f39c607321
Evan OConnor <208516358+Evan-OConnor@users.noreply.github.com>
2026-01-24 18:39:38 +0000

    Configure and verify Spring Boot MySQL integration

---
+++ src/main/java/ie/universityofgalway/projecttrackingsystem/domain/Project.java
@@ -0,0 +1,34 @@
+package ie.universityofgalway.projecttrackingsystem.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    protected Project() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
