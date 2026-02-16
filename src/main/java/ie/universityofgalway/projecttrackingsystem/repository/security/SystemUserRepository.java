adf35742cd0ff061f13776ea3fd5c1404b2a8c06
Evan OConnor <208516358+Evan-OConnor@users.noreply.github.com>
2026-02-16 08:39:00 +0000

    Add security entities and repositories (SystemRole, SystemUser)

---
+++ src/main/java/ie/universityofgalway/projecttrackingsystem/repository/security/SystemUserRepository.java
@@ -0,0 +1,12 @@
+package ie.universityofgalway.projecttrackingsystem.repository.security;
+
+import ie.universityofgalway.projecttrackingsystem.domain.security.SystemUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SystemUserRepository extends JpaRepository<SystemUser, Long> {
+    Optional<SystemUser> findByUsername(String username);
+}
